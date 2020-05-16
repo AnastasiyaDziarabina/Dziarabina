f7808f4be48094b1c4bcc5018bb4f9c67965bfdc
AnastasiyaDziarabina <anastasiyadziarabina@mail.ru>
2020-05-16 13:06:15 +0300

    homeworks

--- src/com/stormnet/homework/homework6/Task3/T3_Test.java
+++
@@ -1,8 +0,0 @@
-package com.stormnet.homework.homework6.Task3;
-
-public class T3_Test {
-    public static void main(String[] args) {
-        T3_Map1 map = new T3_Map1();
-        map.outputKeyAndValue();
-    }
-}
